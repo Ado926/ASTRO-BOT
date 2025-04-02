5143fc58410cd6babe48fe2ca1557bb1c4b34c81
Ado <adonaylester85@gmail.com>
2025-04-02 14:01:42 -0600

    Update herramientas-inspectChannel.js

--- plugins/herramientas-inspectChannel.js
+++ plugins/herramientas-inspectChannel.js
@@ -493,10 +493,10 @@ break
 //const channels = _.values(conn.chats).filter(c => c.id && c.id.endsWith("@newsletter"))
 
 }}
-handler.tags = ['herramientas']
+handler.tags = ['tools']
 handler.help = ['nuevafotochannel', 'nosilenciarcanal', 'silenciarcanal', 'noseguircanal', 'seguircanal', 'avisoschannel', 'resiviravisos', 'inspect', 'inspeccionar', 'eliminarfotochannel', 'reactioneschannel', 'reaccioneschannel', 'nuevonombrecanal', 'nuevadescchannel']
 handler.command = ['nuevafotochannel', 'nosilenciarcanal', 'silenciarcanal', 'noseguircanal', 'seguircanal', 'avisoschannel', 'resiviravisos', 'inspect', 'inspeccionar', 'eliminarfotochannel', 'reactioneschannel', 'reaccioneschannel', 'nuevonombrecanal', 'nuevadescchannel']
-handler.register = true
+handler.register = false
 export default handler 
 
 function formatDate(n, locale = "es", includeTime = true) {
